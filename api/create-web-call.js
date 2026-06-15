e249318888b119c145c754486e0b4607363a2274
Huguitito <huguitito@gmail.com>
2026-06-14 22:12:37 -0300

    Update create-web-call.js

--- api/create-web-call.js
+++ api/create-web-call.js
@@ -1,6 +1,31 @@
 export default async function handler(req, res) {
-  res.status(200).json({
-    status: "ok",
-    message: "API funcionando"
-  });
+
+  try {
+
+    const response = await fetch(
+      "https://api.retellai.com/v2/create-web-call",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${process.env.RETELL_API_KEY}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          agent_id: "agent_92774aab5eb02d8fdf6a3d9a18"
+        })
+      }
+    );
+
+    const data = await response.json();
+
+    res.status(200).json(data);
+
+  } catch (error) {
+
+    res.status(500).json({
+      error: error.message
+    });
+
+  }
+
 }
