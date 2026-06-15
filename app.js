72809f55428ad3b87e4684dc3c17fbe71f428770
Huguitito <huguitito@gmail.com>
2026-06-14 22:26:06 -0300

    Update app.js

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ let retellClient = null;
 
 button.addEventListener("click", async () => {
 
-    if(!active){
+    if (!active) {
 
-        try{
+        try {
 
             active = true;
 
@@ -18,47 +18,70 @@ button.addEventListener("click", async () => {
             statusText.innerText = "Conectando con Blanquita...";
 
             const response = await fetch("/api/create-web-call");
-
             const data = await response.json();
 
-            retellClient = new window.RetellWebClient();
+            console.log("SDK:", window.RetellSDK);
+
+            retellClient =
+                new window.RetellSDK.RetellWebClient();
 
             retellClient.on("call_started", () => {
-                statusText.innerText = "Conversación activa";
+
+                statusText.innerText =
+                    "Conversación activa";
+
             });
 
             retellClient.on("call_ended", () => {
+
                 finalizar();
+
             });
 
             retellClient.on("agent_start_talking", () => {
+
                 orb.classList.add("talking");
-                statusText.innerText = "Blanquita está hablando...";
+
+                statusText.innerText =
+                    "Blanquita está hablando...";
+
             });
 
             retellClient.on("agent_stop_talking", () => {
+
                 orb.classList.remove("talking");
-                statusText.innerText = "Escuchando...";
+
+                statusText.innerText =
+                    "Escuchando...";
+
+            });
+
+            retellClient.on("update", (update) => {
+
+                console.log(update);
+
             });
 
             await retellClient.startCall({
                 accessToken: data.access_token
             });
 
-        }catch(error){
+        } catch (error) {
 
             console.error(error);
 
-            statusText.innerText = "Error de conexión";
+            statusText.innerText =
+                "Error de conexión";
 
             active = false;
 
-            button.innerText = "Iniciar consulta";
+            button.innerText =
+                "Iniciar consulta";
         }
 
-    }else{
+    } else {
 
-        if(retellClient){
+        if (retellClient) {
             retellClient.stopCall();
         }
 
@@ -68,13 +91,15 @@ button.addEventListener("click", async () => {
 
 });
 
-function finalizar(){
+function finalizar() {
 
     active = false;
 
-    button.innerText = "Iniciar consulta";
+    button.innerText =
+        "Iniciar consulta";
 
-    statusText.innerText = "Consulta finalizada";
+    statusText.innerText =
+        "Consulta finalizada";
 
     orb.classList.remove("talking");
 
@@ -85,6 +110,6 @@ function finalizar(){
         statusText.innerText =
             "Presione el botón para comenzar";
 
-    },10000);
+    }, 10000);
 
 }
